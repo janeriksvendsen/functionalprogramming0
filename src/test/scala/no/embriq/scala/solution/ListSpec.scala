29852191d1b75255a2264a3af7dd469d25905764
Jan Erik Svendsen <jan.erik.svendsen@embriq.no>
2015-01-26 13:51:28 +0100

    gjort klar lister til workshop

--- src/test/scala/no/embriq/scala/solution/ListSpec.scala
+++ src/test/scala/no/embriq/scala/solution/ListSpec.scala
@@ -1,10 +1,16 @@
-import no.embriq.scala.exercises.{PendingIfUnimplemented, _}
+package no.embriq.scala.solution
+
+import no.embriq.scala.exercises.PendingIfUnimplemented
 import org.scalatest.{FlatSpec, Matchers}
 
 class ListSpec extends FlatSpec with Matchers with PendingIfUnimplemented {
 
   val list123 = List(1, 2, 3)
   val list456 = List(4, 5, 6)
+
+  "List constructor" should "return same as Cons" in {
+    list123 shouldBe Cons(1, Cons(2, Cons(3, Nil)))
+  }
 
   "List fold right with cons" should "return the same list" in {
     List.foldRight(list123, Nil: List[Int])(Cons(_, _)) shouldBe list123
@@ -28,6 +34,8 @@ class ListSpec extends FlatSpec with Matchers with PendingIfUnimplemented {
 
   "length" should "return the lengt of the list" in {
     List.length(list123) shouldBe 3
+    List.length2(list123) shouldBe 3
+    List.length3(list123) shouldBe 3
   }
 
   "drop while" should "drop start of list while functions evaluate to true" in {
@@ -55,11 +63,17 @@ class ListSpec extends FlatSpec with Matchers with PendingIfUnimplemented {
   "exist" should " retur true when 3 is in the list" in {
     List.exists(list123, (x: Int) => x == 3) shouldBe true
     List.exists(list123, (x: Int) => x == 4) shouldBe false
+
+    List.exists2(list123, (x: Int) => x == 3) shouldBe true
+    List.exists2(list123, (x: Int) => x == 4) shouldBe false
   }
 
   "for all" should "retur true only the list has values under 4" in {
     List.forall(list123, (x: Int) => x == 3) shouldBe false
     List.forall(list123, (x: Int) => x < 4) shouldBe true
+
+    List.forall2(list123, (x: Int) => x == 3) shouldBe false
+    List forall2(list123, (x: Int) => x < 4) shouldBe true
   }
 
   "flatten" should "merge all list in a list to one list" in {
@@ -68,15 +82,21 @@ class ListSpec extends FlatSpec with Matchers with PendingIfUnimplemented {
 
   "filter" should "reteurn a new list with all the elements that evaluates to true" in {
     List.filter(list123, (x: Int) => x > 2) shouldBe List(3)
+    List.filter2(list123, (x: Int) => x > 2) shouldBe List(3)
+    List.filter2(list123, (x: Int) => x < 2) shouldBe List(1)
   }
 
   "map" should "return a new list with the function applied" in {
     List map(list123, (x: Int) => x + 3) shouldBe List(4, 5, 6)
+    List map2(list123, (x: Int) => x + 3) shouldBe List(4, 5, 6)
+
     List.map(list123, (x: Int) => List(x, x)) shouldBe List(List(1, 1), List(2, 2), List(3, 3))
   }
 
   "flatmap" should "return a flattend list" in {
     List.flatMap(list123, (x: Int) => List(x, x)) shouldBe List(1, 1, 2, 2, 3, 3)
+    List.flatMap2(list123, (x: Int) => List(x, x)) shouldBe List(1, 1, 2, 2, 3, 3)
+    List.flatMap3(list123, (x: Int) => List(x, x)) shouldBe List(1, 1, 2, 2, 3, 3)
   }
 
 }
