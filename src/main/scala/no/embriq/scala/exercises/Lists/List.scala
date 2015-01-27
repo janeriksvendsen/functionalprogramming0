384e5de84ecdfffb2ff5df89d0b97be191344180
Jan Erik Svendsen <jan.erik.svendsen@embriq.no>
2015-01-27 10:43:26 +0100

    WIP: Skiller lister til enkel rekursiv kode og høyereordends kode

--- src/main/scala/no/embriq/scala/exercises/Lists/List.scala
+++ src/main/scala/no/embriq/scala/exercises/Lists/List.scala
@@ -1,4 +1,4 @@
-package no.embriq.scala.exercises.Lists
+package no.embriq.scala.exercises.lists
 
 sealed trait List[+A]
 
@@ -11,6 +11,8 @@ object List {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
+  def apply[A](as: scala.List[A]): List[A] =  as.foldRight(Nil:List[A])( (a:A, b:List[A]) => Cons(a,b))
+
   def sum(ints: List[Int]): Int = ???
 
   def product(ds: List[Int]): Double = ???
@@ -18,44 +20,18 @@ object List {
   // returner de n første elementer i lista
   def take[A](l: List[A], n: Int): List[A] = ???
 
-  // returner alle i lista frm til funksjonen returnere true
-  def takeWhile[A](l: List[A], f: A => Boolean): List[A] = ???
-
   // returner resten av lista fra pos n.
   def drop[A](l: List[A], n: Int): List[A] = ???
-
-  // hopp over de første elementene i listen så lenge funksjonen returnerer true
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
 
   def length[A](as: List[A]): Int = ???
 
   // Slå sammen to lister til 1
   def append[A](a1: List[A], a2: List[A]): List[A] = ???
 
-  // fra høyre til venstre
-  def foldRight[A, B](l: List[A], z: B)(f: (A, B) => B): B = ???
-
-  // fra venstre til høyre
-  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = ???
-
-  def filter[A](l: List[A], f: A => Boolean): List[A] = ???
-
   def reverse[A](l: List[A]): List[A] = ???
 
   // gjør om en liste av lister til en liste med elementen i sub listen.
   //  List.flatten( List(List(1,2,3), List(4,5,6))) == List(1,2,3,4,5,6)
   def flatten[A](l: List[List[A]]): List[A] = ???
-
-  // return er en ny liste som er resultatet av f applied på alle elementetn i listen.
-  def map[A, B](l: List[A], f: A => B): List[B] = ???
-
-
-  def flatMap[A, B](as: List[A], f: A => List[B]): List[B] = ???
-
-  // true iff f == true on all elements
-  def forall[A](l: List[A], f: A => Boolean): Boolean = ???
-
-  // true iff f == true on any elements
-  def exists[A](l: List[A], f: A => Boolean): Boolean = ???
 
 }
--- src/main/scala/no/embriq/scala/solution/Lists/List.scala
+++ src/main/scala/no/embriq/scala/solution/Lists/List.scala
@@ -1,6 +1,4 @@
-package no.embriq.scala.solution.Lists
-
-import scala.annotation.tailrec
+package no.embriq.scala.solution.lists
 
 sealed trait List[+A]
 
@@ -24,22 +22,24 @@ object List {
     case Cons(h, t) => Cons(h, append(t, a2))
   }
 
-  def flatten[A](l: List[List[A]]): List[A] = {
-    List.foldRight(l, Nil: List[A])(append)
+  def flatten[A](l: List[List[A]]): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, t) => append(h, flatten(t))
   }
 
-  def sum(l: List[Int]): Int = List.foldLeft(l, 0)(_ + _)
+  def sum(l: List[Int]): Int = l match {
+    case Nil => 0
+    case Cons(h, t) => h + sum(t)
+  }
 
-  def product(l: List[Int]): Int = List.foldLeft(l, 1)(_ * _)
+  def product(l: List[Int]): Int = l match {
+    case Nil => 1
+    case Cons(h, t) => h * product(t)
+  }
 
   def take[A](l: List[A], n: Int): List[A] = l match {
     case Nil => Nil
     case Cons(h, t) => if (n == 0) Nil else Cons(h, take(t, n - 1))
-  }
-
-  def takeWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
-    case Nil => Nil
-    case Cons(h, t) => if (f(h)) Cons(h, takeWhile(t, f)) else Nil
   }
 
   def drop[A](l: List[A], n: Int): List[A] = l match {
@@ -47,72 +47,11 @@ object List {
     case Cons(h, t) => if (n == 0) l else drop(t, n - 1)
   }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
-    case Nil => Nil
-    case Cons(h, t) => if (f(h)) dropWhile(t, f) else l
-  }
-
   def length[A](l: List[A]): Int = l match {
     case Nil => 0
     case Cons(h, t) => 1 + length(t)
   }
 
-  def length2[A](l: List[A]): Int = List.foldLeft(l, 0)((i, _) => i + 1)
-
-  def length3[A](l: List[A]): Int = List.foldRight(l, 0)((_, i) => i + 1)
-
-  def foldRight[A, B](l: List[A], z: B)(f: (A, B) => B): B = l match {
-    case Nil => z
-    case Cons(h, t) => f(h, foldRight(t, z)(f))
-  }
-
-  @tailrec
-  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
-    case Nil => z
-    case Cons(h, t) => foldLeft(t, f(z, h))(f)
-  }
-
-  def reverse[A](l: List[A]): List[A] = List.foldLeft(l, Nil: List[A])((t, h) => Cons(h, t))
-
-  def filter[A](l: List[A], f: A => Boolean): List[A] = l match {
-    case Nil => Nil
-    case Cons(h, t) => if (f(h)) Cons(h, filter(t, f)) else filter(t, f)
-  }
-
-  def filter2[A](l: List[A], f: A => Boolean): List[A] = this.foldRight(l, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
-
-  def filter3[A](l: List[A])( f: A => Boolean): List[A] = this.foldRight(l, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
-
-  def map[A, B](l: List[A], f: A => B): List[B] = l match {
-    case Nil => Nil
-    case Cons(h, t) => Cons(f(h), map(t, f))
-  }
-
-  def map2[B, A](l: List[A], f: A => B): List[B] = this.foldRight(l, Nil: List[B])((h, t) => Cons(f(h), t))
-
-  def flatMap[A, B](l: List[A], f: A => List[B]): List[B] = l match {
-    case Nil => Nil
-    case Cons(h, t) => List.append(f(h), flatMap(t, f))
-  }
-
-  def flatMap2[A, B](l: List[A], f: A => List[B]): List[B] = List.flatten(List.map(l, f))
-
-  def flatMap3[A, B](l: List[A], f: A => List[B]): List[B] = this.foldRight(l, Nil: List[B])((h, t) => List.append(f(h), t))
-
-  //  true iff f == true on all elements
-  def forall[A](l: List[A], f: A => Boolean): Boolean = l match {
-    case Nil => true
-    case Cons(h, t) => if (f(h)) forall(t, f) else false
-  }
-
-  def forall2[A](l: List[A], f: A => Boolean): Boolean = foldLeft(l, true)(_ && f(_))
-
-  // true iff f == true on any elements
-  def exists[A](l: List[A], f: A => Boolean): Boolean = l match {
-    case Nil => false
-    case Cons(h, t) => if (f(h)) true else exists(t, f)
-  }
-
-  def exists2[A](l: List[A], f: A => Boolean): Boolean = foldLeft(l, false)(_ || f(_))
+  def reverse[A](l: List[A]): List[A] = ???
 
 }
